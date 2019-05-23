acf01bec4cad3c1ecf87fa7be24d9f9351075842
Sergei Vizel <sergei.vizel@modera.net>
2019-05-23 13:49:54 +0300

    sf4 compatibility

--- Tests/Fixtures/Bundle/DependencyInjection/ModeraLanguagesDummyExtension.php
+++ Tests/Fixtures/Bundle/DependencyInjection/ModeraLanguagesDummyExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modera\LanguagesBundle\DependencyInjection;
+namespace Modera\LanguagesBundle\Tests\Fixtures\Bundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -12,25 +12,14 @@ use Symfony\Component\Config\FileLocator;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ModeraLanguagesExtension extends Extension
+class ModeraLanguagesDummyExtension extends Extension
 {
-    const CONFIG_KEY = 'modera_languages.config';
-
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $container->setParameter(self::CONFIG_KEY, $config);
-        foreach ($config as $key => $value) {
-            $container->setParameter(self::CONFIG_KEY.'.'.$key, $value);
-        }
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
 
         if (class_exists('Symfony\Component\Console\Application')) {
             try {
